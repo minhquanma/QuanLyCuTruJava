7b7dadf1f6080b36bb099517b1815bce596f5f76
minhquanma <minhquanma@gmail.com>
2018-10-08 17:04:45 +0700

    Implemented UI for DangKyCuTru

--- Frontend/QuanLyCuTru/src/app/pages/dang-ky-cu-tru/dang-ky-cu-tru.component.ts
+++ Frontend/QuanLyCuTru/src/app/pages/dang-ky-cu-tru/dang-ky-cu-tru.component.ts
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
 import { AppComponent } from "../../app.component";
 import { CutruService } from "../../services/cutru.service";
 import { UtilityService } from "../../services/utility.service";
 import { NguoiDung } from "../../models/standards/nguoidung";
-=======
-import { Component, OnInit } from '@angular/core';
-import { AppComponent } from '../../app.component';
-import { CutruService } from '../../services/cutru.service';
-import { UtilityService } from '../../services/utility.service';
->>>>>>> 664fe59436ec8b1e13a58c47f330b0ac3e0ad119
 
 @Component({
-  selector: 'app-dang-ky-cu-tru',
-  templateUrl: './dang-ky-cu-tru.component.html',
-  styleUrls: ['./dang-ky-cu-tru.component.scss']
+  selector: "app-dang-ky-cu-tru",
+  templateUrl: "./dang-ky-cu-tru.component.html",
+  styleUrls: ["./dang-ky-cu-tru.component.scss"]
 })
 export class DangKyCuTruComponent extends AppComponent implements OnInit {
   public ngayTao: Date = new Date();
@@ -22,13 +15,13 @@ export class DangKyCuTruComponent extends AppComponent implements OnInit {
   public ngayHetHan: Date = new Date();
 
   public readonly loaiCuTrus = [
-    { name: 'Tạm vắng', value: true },
-    { name: 'Tạm trú', value: false }
+    { name: "Tạm vắng", value: 1 },
+    { name: "Tạm trú", value: 2 }
   ];
   public bsValue: Date = new Date();
 
   private selectedLoaiCuTru: number;
-  private congDanList: NguoiDung[];
+  public congDanList: String[] = [];
 
   constructor(
     protected cuTruService: CutruService,
@@ -41,6 +34,14 @@ export class DangKyCuTruComponent extends AppComponent implements OnInit {
 
   ngOnInit() {
     this.ngayHetHan.setMonth(this.ngayHetHan.getMonth() + 1);
+  }
+
+  public onAddCongDanClicked(value): void {
+    this.congDanList.push(value);
+  }
+
+  public onLoaiCuTruSelectChanged(value: number): void {
+    console.log(value);
   }
 
   public submit(value) {
